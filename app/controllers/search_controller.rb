dc47dd2e8d06094da02a767e6234dbce14fd5663
Bryan Goss <bcgoss@gmail.com>
2016-10-19 09:46:37 -0600

    add views and adjust controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,3 +1,6 @@
 class SearchController < ApplicationController
 
+  def index
+    @stations = [{name: 'Test', address: '123 Fake St.'}, {name: 'Test', address: '123 Fake St.'}]
+  end
 end
--- spec/features/user_can_search_by_zipcode_spec.rb
+++ spec/features/user_can_search_by_zipcode_spec.rb
@@ -12,8 +12,12 @@ describe 'Search' do
    #Then I should be on page "/search" with parameters visible in the url
     expect(current_path).to eq('/search')
    #Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+    expect(page).to have_content('Nearest Stations')
    #And the stations should be limited to Electric and Propane
+    expect(page).to_not have_content('Hydrogen')
+    expect(page).to_not have_content('LNG')
    #And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
-
+    expect(page).to have_content('Name: ')
+    expect(page).to have_content('Address: ')
   end
 end
